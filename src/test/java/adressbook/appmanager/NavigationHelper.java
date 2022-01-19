dfba56b8f3dc3a8ffb37089a0b7fd19065193ec0
SergeMar <marchenich@mail.ru>
2022-01-19 22:41:58 +0300

    Оптимизация кода

--- src/test/java/adressbook/appmanager/NavigationHelper.java
+++ src/test/java/adressbook/appmanager/NavigationHelper.java
@@ -3,14 +3,13 @@ package adressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper {
-    private static WebDriver driver;
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
     public void goToGroupPage() {
-        driver.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
     }
 }
--- src/test/java/adressbook/appmanager/SessionHelper.java
+++ src/test/java/adressbook/appmanager/SessionHelper.java
@@ -3,21 +3,15 @@ package adressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class SessionHelper {
-    private WebDriver driver;
+public class SessionHelper extends HelperBase {
 
     public SessionHelper(WebDriver driver) {
-
-        this.driver = driver;
+        super(driver);
     }
 
-    public void login(String admin, String password) {
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys(admin);
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys(password);
-        driver.findElement(By.xpath("//input[@value='Login']")).click();
+    public void login(String username, String password) {
+        type(By.name("user"), username);
+        type(By.name("pass"), password);
+        click(By.xpath("//input[@value='Login']"));
     }
 }
