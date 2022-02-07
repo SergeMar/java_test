dafda3c33c2157ad02a96711e9c8634353379608
SergeMar <marchenich@mail.ru>
2022-02-07 17:38:15 +0300

    Использование компаратора и лямбда при создании групп

--- addressbook/src/test/java/adressbook/tests/GroupCreationTests.java
+++ addressbook/src/test/java/adressbook/tests/GroupCreationTests.java
@@ -1,10 +1,10 @@
 package adressbook.tests;
 
 import adressbook.model.GroupData;
-import jdk.dynalink.linker.LinkerServices;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,17 +14,13 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
         app.getNavigationHelper().goToGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("test1", "test2", null);
+        GroupData group = new GroupData("test2", "test2", null);
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assertions.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (GroupData g : after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
+        int max1 = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId(); //Лямбда выражение
+        int max = after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId();
         group.setId(max);
         before.add(group);
         Assertions.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
